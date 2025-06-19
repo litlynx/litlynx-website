6f428898a18944434d8d7f1a86ec5bd8e0f2b703
Jorge Miranda <github@litlynx.com>
2025-06-19 15:46:00 +0100

    fix: Update site URL to include 'www' prefix in Astro configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://litlynx.com",
+  site: "https://www.litlynx.com",
   integrations: [tailwind(), vue(), sitemap()],
   output: "static",
   build: {
