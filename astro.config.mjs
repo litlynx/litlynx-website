724112817cfd7bc63a8eed602105eef13dda091a
Jorge Miranda <github@litlynx.com>
2025-06-19 12:50:47 +0100

    feat: Update Astro configuration to use static output and remove Vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,15 +2,14 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
-
-import vercel from "@astrojs/vercel";
-
 import vue from "@astrojs/vue";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://litlynx.com",
   integrations: [tailwind(), vue()],
-  output: "server",
-  adapter: vercel(),
+  output: "static",
+  build: {
+    format: "directory",
+  },
 });
